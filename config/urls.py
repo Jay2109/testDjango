1ed4943d02723c47aae2eefb1e1127c610f3fdd4
Jay Jain <jayjain2109@gmail.com>
2021-02-24 23:51:20 +0530

    testing

--- config/urls.py
+++ config/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     re_path(r"^comments/", include("django_comments_xtd.urls")),
 ]
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
