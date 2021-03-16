a0152926a893b11e2aa11381ca31954c08ed49eb
Aadesh <aadeshkarira01@gmail.com>
2021-03-16 15:56:15 +0530

    commit

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "comments",
     "notifications",
     "corsheaders",
+    "django_extensions",
 ]
 
 THIRD_PARTY_APPS = [
@@ -91,8 +92,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'qaproject',
+        "USER": 'root',
+        "PASSWORD": '', 
+        "PORT": 3306,
+        "HOST": '127.0.0.1',
     }
 }
 
